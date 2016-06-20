743463c6884a775514db254faef17be9a61eec5f
Zhangqibin <zhangqibin2008@gmail.com>
2016-06-20 16:45:10 +0800

    simulationCenter

--- LiFangSport/Classes/Controller/SimulationTest/SimulationCenterVC.m
+++ LiFangSport/Classes/Controller/SimulationTest/SimulationCenterVC.m
@@ -9,30 +9,72 @@
 #import "SimulationCenterVC.h"
 
 @interface SimulationCenterVC ()
+    <UITableViewDelegate, UITableViewDataSource>
+
+@property (nonatomic, strong) UITableView *tableView;
+@property (nonatomic, strong) NSMutableArray *dataArray;
 
 @end
 
 @implementation SimulationCenterVC
 
-- (void)viewDidLoad {
+#pragma mark - View Life Cycle
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-
-    self.title = @"规则测试";
+    
+    self.title = @"模拟测试";
+    [self.view addSubview:self.tableView];
+    NSDictionary *views = NSDictionaryOfVariableBindings(_tableView);
+    NSArray *tanArray = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[_tableView]-0-|" options:0 metrics:0 views:views];
+    NSArray *tanArray2 = [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[_tableView]-0-|" options:0 metrics:0 views:views];
+    
+    [self.view addConstraints:tanArray];
+    [self.view addConstraints:tanArray2];
 }
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma mark - UITableViewDelegate and UITableViewDataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 2;
+    return self.dataArray.count;
 }
-*/
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *customCellID = @"customCellID";
+    
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:customCellID];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:customCellID];
+    }
+    cell.textLabel.text = [NSString stringWithFormat:@"%@", @(indexPath.row)];
+    
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+}
+
+#pragma mark - Getter and Setter
+- (UITableView *)tableView
+{
+    if (!_tableView) {
+        _tableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStyleGrouped];
+        _tableView.delegate = self;
+        _tableView.dataSource = self;
+        _tableView.separatorStyle = UITableViewCellSeparatorStyleSingleLine;
+    }
+    return _tableView;
+}
 
 @end

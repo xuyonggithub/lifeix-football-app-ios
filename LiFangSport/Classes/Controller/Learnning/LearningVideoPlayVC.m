13680dbabe8882979ff7c1462fbc81ca106a9e71
zy <525153575@qq.com>
2016-06-22 17:50:23 +0800

    增加video播放类

---
+++ LiFangSport/Classes/Controller/Learnning/LearningVideoPlayVC.m
@@ -0,0 +1,47 @@
+//
+//  LearningVideoPlayVC.m
+//  LiFangSport
+//
+//  Created by 张毅 on 16/6/22.
+//  Copyright © 2016年 zhangyi. All rights reserved.
+//
+
+#import "LearningVideoPlayVC.h"
+#import "AJMediaPlayerKit.h"
+
+@interface LearningVideoPlayVC ()<AJMediaViewControllerDelegate>
+{
+    BOOL _isFullScreen;
+}
+
+//播放器
+@property (nonatomic, strong) AJMediaPlayerViewController *mediaPlayerViewController;
+@property (nonatomic, strong) UIView *disPlayView;
+@property (nonatomic, strong) NSMutableArray *constraintList;
+@property (nonatomic, strong) AJMediaPlayRequest *playRequest;
+
+@end
+
+@implementation LearningVideoPlayVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

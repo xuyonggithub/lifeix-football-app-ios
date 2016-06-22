e1daf7a5b162f7d1493b136f15ff71ceee9d7159
卢亚林 <luyalin@luyalindeMacBook-Pro.local>
2016-06-22 17:12:39 +0800

    新增球员详情

---
+++ LiFangSport/Classes/Controller/Player/WomanPlayerDetailVC.h
@@ -0,0 +1,13 @@
+//
+//  WomanPlayerDetailVC.h
+//  LiFangSport
+//
+//  Created by 卢亚林 on 16/6/22.
+//  Copyright © 2016年 zhangyi. All rights reserved.
+//
+
+#import "BaseVC.h"
+
+@interface WomanPlayerDetailVC : BaseVC
+
+@end

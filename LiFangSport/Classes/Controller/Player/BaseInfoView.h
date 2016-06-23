872f14daa7c309150277f8121bed92aebb984b66
卢亚林 <luyalin@luyalindeMacBook-Pro.local>
2016-06-23 12:06:01 +0800

    球员基本信息

---
+++ LiFangSport/Classes/Controller/Player/BaseInfoView.h
@@ -0,0 +1,13 @@
+//
+//  BaseInfoView.h
+//  LiFangSport
+//
+//  Created by 卢亚林 on 16/6/23.
+//  Copyright © 2016年 zhangyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseInfoView : UIView
+
+@end

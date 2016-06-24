9a80229eb7585f69e4dff62a83485b2db67f5103
卢亚林 <luyalin@luyalindeMacBook-Pro.local>
2016-06-24 11:16:58 +0800

    教练详情

---
+++ LiFangSport/Classes/Controller/Coach/CoachInfoView.h
@@ -0,0 +1,13 @@
+//
+//  CoachInfoView.h
+//  LiFangSport
+//
+//  Created by 卢亚林 on 16/6/24.
+//  Copyright © 2016年 zhangyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CoachInfoView : UIView
+
+@end
---
+++ LiFangSport/Classes/Controller/Coach/CoachInfoView.m
@@ -0,0 +1,21 @@
+//
+//  CoachInfoView.m
+//  LiFangSport
+//
+//  Created by 卢亚林 on 16/6/24.
+//  Copyright © 2016年 zhangyi. All rights reserved.
+//
+
+#import "CoachInfoView.h"
+
+@implementation CoachInfoView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end

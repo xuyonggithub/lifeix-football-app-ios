f2d785ee7cf09522ac839b8cdcf6c2b0f9c68063
zy <525153575@qq.com>
2016-06-27 17:58:49 +0800

    实时比分

---
+++ LiFangSport/Classes/Controller/CurrentlyScore/CurrentlyScoreVC.h
@@ -0,0 +1,13 @@
+//
+//  CurrentlyScoreVC.h
+//  LiFangSport
+//
+//  Created by 张毅 on 16/6/27.
+//  Copyright © 2016年 zhangyi. All rights reserved.
+//
+
+#import "BaseMenuCenterVC.h"
+
+@interface CurrentlyScoreVC : BaseMenuCenterVC
+
+@end
---
+++ LiFangSport/Classes/Controller/CurrentlyScore/CurrentlyScoreVC.m
@@ -0,0 +1,37 @@
+//
+//  CurrentlyScoreVC.m
+//  LiFangSport
+//
+//  Created by 张毅 on 16/6/27.
+//  Copyright © 2016年 zhangyi. All rights reserved.
+//
+
+#import "CurrentlyScoreVC.h"
+
+@interface CurrentlyScoreVC ()
+
+@end
+
+@implementation CurrentlyScoreVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

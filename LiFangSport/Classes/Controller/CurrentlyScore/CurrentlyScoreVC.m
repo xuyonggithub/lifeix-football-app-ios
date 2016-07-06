bf095a8225e09d2c8d7aaeb9be1e9b7ca5bd67cf
zy <525153575@qq.com>
2016-07-06 09:45:40 +0800

    实时比分

--- LiFangSport/Classes/Controller/CurrentlyScore/CurrentlyScoreVC.m
+++ LiFangSport/Classes/Controller/CurrentlyScore/CurrentlyScoreVC.m
@@ -7,8 +7,11 @@
 //
 
 #import "CurrentlyScoreVC.h"
+#import "LeftSwitchCell.h"
 
-@interface CurrentlyScoreVC ()
+@interface CurrentlyScoreVC ()<UITableViewDataSource,UITableViewDelegate>
+@property(nonatomic,strong)UITableView *kTableview;
+@property(nonatomic,strong)NSMutableArray *dataArray;
 
 @end
 
@@ -16,22 +19,64 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self.view addSubview:self.kTableview];
+    
 }
+
+#pragma mark - UITableViewDelegate and UITableViewDataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 10;//self.dataArray.count;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 25 + (SCREEN_WIDTH - 25) / 2.0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *customCellID = @"LeftSwitchCellid";
+    
+    LeftSwitchCell * cell = [tableView dequeueReusableCellWithIdentifier:customCellID];
+    if (!cell) {
+        cell = [[LeftSwitchCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:customCellID];
+    }
+//    [cell refreshContent:self.dataArray[indexPath.row]];
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+   
+}
+
+#pragma mark - Getter and Setter
+-(UITableView *)kTableview{
+    if (!_kTableview) {
+        _kTableview = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStylePlain];
+        _kTableview.backgroundColor = [UIColor lightGrayColor];
+        _kTableview.delegate = self;
+        _kTableview.dataSource = self;
+//        _kTableview.separatorStyle = UITableViewCellSeparatorStyleNone;
+//        _kTableview.tableFooterView = [UIButton buttonWithType:UIButtonTypeCustom];
+    }
+    return _kTableview;
+}
+-(NSMutableArray *)dataArray{
+    if (!_dataArray) {
+        _dataArray = [NSMutableArray new];
+    }
+    return _dataArray;
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end

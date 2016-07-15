e2fd1d0695d82533a4d41b7299f7af4de117d4ca
Zhangqibin <zhangqibin2008@gmail.com>
2016-07-15 13:49:50 +0800

    t

---
+++ LiFangSport/Classes/Controller/Base/CollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.m
+//  LiFangSport
+//
+//  Created by Zhangqibin on 16/7/15.
+//  Copyright © 2016年 zhangyi. All rights reserved.
+//
+
+#import "CollectionViewCell.h"
+
+@implementation CollectionViewCell
+
+@end
